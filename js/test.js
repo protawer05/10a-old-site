394f88bead6cee2f3e596937d095a15b6de74754
Anton <sychiov2015@yandex.ru>
2022-03-30 00:37:21 +0500

    #

---
+++ js/test.js
@@ -0,0 +1,9 @@
+const submit = document.querySelector('.submit'),
+      text = document.querySelector('.text');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    localStorage.setItem(text.value, true);
+});
+localStorage.clear()
